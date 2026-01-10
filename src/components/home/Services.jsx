7d04f752b8c89df8bbd9ceef7b83e2c390408f00
Shuvo Roy <sr9891710@gmail.com>
2026-01-10 18:47:37 +0600

    responsive

--- src/components/home/Services.jsx
+++ src/components/home/Services.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import services from "../../../public/data/services.json";
 import BookingDialog from "./BookingDialog";
 import { motion } from "motion/react";
@@ -14,16 +12,36 @@ export default function Services() {
         infinite: true,
         speed: 500,
         slidesToShow: 4,
-        // slidesToScroll: 1,
+        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
         pauseOnHover: true,
         responsive: [
-            { breakpoint: 1024, settings: { slidesToShow: 3 } },
-            { breakpoint: 768, settings: { slidesToShow: 2 } },
-            { breakpoint: 480, settings: { slidesToShow: 1, dots: false } },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false,
+                },
+            },
         ],
     };
+
 
     return (
         <div>

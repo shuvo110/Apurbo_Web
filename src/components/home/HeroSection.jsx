09a0efd5ed5cc094756d3f6c4af28eccf11d9f23
Shuvo Roy <sr9891710@gmail.com>
2026-01-11 07:27:54 +0600

    flexLg

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -9,7 +9,7 @@ function HeroSection() {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1.6, ease:"easeOut" }}
                 className=" bg-base-200 py-18 mt-7 md:mt-19">
-                <div className="md:flex items-center p-4 justify-center gap-28 lg:flex-row-reverse">
+                <div className="lg:flex items-center p-4 justify-center gap-28 lg:flex-row-reverse">
                     <div>
                         <img
                             src="https://scontent.fdac198-1.fna.fbcdn.net/v/t39.30808-6/518423345_122136786236421209_6325512940476710924_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=103&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeEf29NOZmgkEX8l1hUM90l34lVOhUtBk0HiVU6FS0GTQe77iBGkDpbNno1w2Z1pAr4QgCWmyI5E810w2nwvPuAh&_nc_ohc=YahuAJCg1ZUQ7kNvwELc41V&_nc_oc=AdkD2BiTjyZTi9K_GdOvlzzDQVtYEI5vbI8nT6iHzpie245I-8Gxk4oIvd8ygO2c_MQ&_nc_zt=23&_nc_ht=scontent.fdac198-1.fna&_nc_gid=sSsTK0AwQsKN0rWdf8w6mA&oh=00_AfrjGuKKWXKWGcjB0iGc9Z4E-MtJT-uSaks18SfFCJP0Jg&oe=6968DED5"
